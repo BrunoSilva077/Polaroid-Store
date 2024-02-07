679880f4f4d6abf3c932857575cd8a60d5405ac1
BrunoSilva077 <brunosilva1012003@gmail.com>
2024-02-07 00:01:33 +0000

    primeiro commit, navbar iniciada

---
+++ Polaroid/src/pages/NavBar.jsx
@@ -0,0 +1,14 @@
+export default function NavBar() {
+  return (
+    <div className="grid-container">
+        {/* NavBar */}
+        <div className="item item2">
+            <div className="logo">
+                <a>Polaroid</a>
+            </div>
+        </div>
+        <div className="item item6">6</div>
+        <div className="item item4">Admin Shop About Contact</div>
+    </div>
+  )
+}
