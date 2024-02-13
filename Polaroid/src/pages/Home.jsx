c4767108098fbca0e25a21f6f4e5de7039ebd6a8
BrunoSilva077 <brunosilva1012003@gmail.com>
2024-02-13 22:28:46 +0000

    home page created

--- Polaroid/src/pages/Home.jsx
+++ Polaroid/src/pages/Home.jsx
@@ -4,7 +4,19 @@ export default function Home() {
   return (
     <>
         <NavBar />
-        <div>Home</div>
+        <div className="grid-container">
+          <div className="item item1"></div>
+          <div className="item item3">
+            <div className="flex flex-col tittle">
+              <h1>The Original is back</h1>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui ex quidem tempora adipisci molestias debitis.</p>
+              <button className="btn btn-shop">Shop</button>
+            </div>
+          </div>
+          <div className="item item7 img">
+            <img src="public//Polaroid-removebg.png" alt="polaroid home" />
+          </div>
+        </div>
     </>
   )
 }
--- Polaroid/src/pages/NavBar.jsx
+++ Polaroid/src/pages/NavBar.jsx
@@ -1,14 +1,18 @@
 export default function NavBar() {
   return (
     <div className="grid-container">
-        {/* NavBar */}
         <div className="item item2">
             <div className="logo">
                 <a>Polaroid</a>
             </div>
         </div>
-        <div className="item item6">6</div>
-        <div className="item item4">Admin Shop About Contact</div>
+        <div className="item item6"></div>
+        <div className="item item4 options">
+          <a>Admin</a>
+          <a>Shop</a>
+          <a>About</a>
+          <a >Contact</a>
+        </div>
     </div>
   )
 }
